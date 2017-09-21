a036de9e4449915598deb1f62d3c301754b9cbf7
DESKTOP-FU1HM40\Qasid Labeed <qasid.labeed@blockchainexpertsolutions.com>
2017-09-21 08:03:29 -0700

    no message

---
+++ Restful API test/Api.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Globalization;
+using Numerics;
+using Nethereum.Web3;
+
+namespace Restful_API_test
+{
+    
+    public class Api
+    {
+        /* This function is used to send http request to API*/
+        public string CallApi(string url) {
+
+            string Result = string.Empty;
+           
+            HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(url);
+            Request.KeepAlive = false;
+            Request.Method = "GET"; // Would be POST for some methods 
+            
+            //For SSL/TLS exception
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)Request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    Result = reader.ReadToEnd();
+                }
+
+                //To Get Result in Hex Value 
+                var HexValue = GetStringResult(GetJsonObj(Result));
+
+                //To Convert Hex To Decimal
+                var DecValue = HexToDecString(HexValue);
+                return Result;
+
+            }
+
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                    Console.WriteLine(errorText);
+                }
+                throw;
+            }            
+        }
+
+        public JObject GetJsonObj(string result)
+        {
+            var JObj = JObject.Parse(result);
+            return JObj;
+        }
+
+        public string GetStringResult(JObject res) {
+
+            /*For result containing multiple values can be extracted by calling the relevant index
+            * e.g (string)res["result"]["nounce"] */
+            string Str = Convert.ToString(res["result"]);
+            return Str;
+        }
+
+        public string HexToDecString(string val) {
+
+            //Removes the '0x' from the result to passed to BigInteger
+            var str = val.Substring(2, (val.Length) - 2);
+
+            BigInteger dec = new BigInteger(str, 16);
+            var ToString = dec.ToString();
+            return ToString;
+        }
+
+        public Double StringToEther(string str)
+        {
+            Web3 web3 = new Web3();
+            var Wei = web3.Convert.ToWei(str, UnitConversion.EthUnit.Wei);
+
+            //Balance divided by 10^18
+            var Balance = new BigRational(Wei, 1000000000000000000); 
+            return (double)Balance;
+
+        }
+    }
+}
---
+++ Restful API test/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Numerics;
+using Nethereum.Web3;
+
+
+
+
+namespace Restful_API_test
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string network = "rinkeby"; // Enter Network Here e.g mainnet, rinkeby, ropsten, Kovan,infuranet
+
+            /*For GET request
+             * "web3_clientVersion","net_version","net_listening","net_peerCount","eth_protocolVersion","eth_syncing","eth_mining","eth_hashrate",
+             * "eth_gasPrice","eth_accounts","eth_blockNumber","eth_getBalance","eth_getStorageAt",
+             * "eth_getTransactionCount","eth_getBlockTransactionCountByHash",
+             * "eth_getBlockTransactionCountByNumber","eth_getUncleCountByBlockHash","eth_getUncleCountByBlockNumber",
+             * "eth_getCode","eth_call","eth_getBlockByHash","eth_getBlockByNumber",
+             * "eth_getTransactionByHash","eth_getTransactionByBlockHashAndIndex",
+             * "eth_getTransactionByBlockNumberAndIndex","eth_getTransactionReceipt",
+             * "eth_getUncleByBlockHashAndIndex","eth_getUncleByBlockNumberAndIndex",
+             * "eth_getCompilers","eth_getLogs","eth_getWork","parity_pendingTransactions"
+             
+            For POST request
+            * "eth_sendRawTransaction","eth_estimateGas","eth_submitWork","eth_submitHashrate" */
+            // supported methods mentioned above
+            string method = "eth_getBalance";
+
+            //change the value based on number of parameters
+            string[] parameters = new string[2];
+
+            // leave it empty for methods that don't require parameters
+            //More Parameters can be added (Any Added parameter also needs to be added to the url string 
+            //e.g "\"" + parameters[number] + "\"" 
+            parameters[0] = "0x29bf8bE493122F010FCD9db8d6466a8F8B1da7F0"; 
+            parameters[1] = "latest";
+            //parameter[2]="";
+            
+
+            /*API format
+            https://api.infura.io/v1/jsonrpc/network/method?parms= */
+
+            string url = "https://api.infura.io/v1/jsonrpc/"+ network + "/" + method + "?params=" + "[" + "\"" + parameters[0] + "\"" + "," + "\"" + parameters[1] + "\"" + "]";
+            Api api = new Api();
+            var ApiResult = api.CallApi(url);
+
+            //String value from Api call is converted to JSON object then [result] is extracted
+            var JsonObject = api.GetJsonObj(ApiResult);
+            var StringResult = api.GetStringResult(JsonObject);
+
+            //To Convert hex values to Decimal
+            var Str = api.HexToDecString(StringResult);
+
+            //To Convert to Ether Balance
+            Double Balance = api.StringToEther(Str);
+
+            //To print the final result
+            Console.WriteLine("Your Balance in Ether = " + Balance +" Ethers"); 
+            Console.ReadKey();
+
+        }
+    }
+}
